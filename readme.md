252c690e4ad256974a7d3a75653e6149abf96a5e
Maxx <maxhiga.hawaii@gmail.com>
2020-10-17 11:40:05 -1000

    commit of HW3 pass word gen

---
+++ readme.md
@@ -0,0 +1,9 @@
+Homework 3 Javascript: Random Password Generator
+
+For this weeks homework, we had to make a random password generator.
+
+when using my code in a browser, press the button to start the process, it will ask for you to type how long the password will be (8, 9, 10, etc) in terms of character length.
+
+then it will ask through a series of prompts what type of characters you want in it including lower case letters, special characters, numbers, and upper case characters. after that it will tell you how long and what characters will be implemented.
+
+if you did  not type in a digit between 8 and 128 for the password length it will send an error message and return to home screen. if you dont choose any character types it will return to homescreen
---
+++ script.js
@@ -0,0 +1,92 @@
+// variables for the password, all the types of characters
+var uppCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specCharac = "`~!@#$%^&*()_+-=[]{}\|:'<>?,./;"
+
+// the variables for what is selected and what is the password
+var userSelect = [];
+var userPass = [];
+
+
+
+// Write password to the #password input
+function writePassword() {
+
+  let length = prompt("Choose between 8 and 128 digits for your password");
+  if(length < 8 || length > 128) {
+    alert("Sorry but your password must be between 8 and 128 digits. Also make sure you are typing in numbers only.");
+    return;
+  }
+  console.log("length of your password will be: " + length);
+
+  let upper = confirm("Would you like upper case letters in your password?");
+  if(upper) {
+    userSelect.push(...uppCase);
+  }
+  console.log("all possible characters after choosing the first group are: " + userSelect);
+
+  let lower = confirm("Would you like lower case letters in your password?");
+  if(lower) {
+    userSelect.push(...lowCase);
+  }
+  console.log("all possible characters after choosing the 2 groups are: " + userSelect);
+
+  let digits = confirm("Would you like numbers in your password?");
+  if(digits) {
+    userSelect.push(...numbers);
+  }
+  console.log("all possible characters after choosing the 3 groups are: " + userSelect);
+
+  let special = confirm("Would you like special characters in your password?");
+  if(special) {
+    userSelect.push(...specCharac);
+  }
+  console.log("all possible characters after choosing the 4 groups are: " + userSelect);
+
+  if (upper === false && lower === false && digits === false && special === false) {
+    alert("Sorry, you need to have at least one of the different character types in your password.")
+    return;
+  }
+
+  if(upper) {
+    var upMessage = (" and upper case characters")
+  }  else {
+    var upMessage = ("")
+  }
+
+  if(lower) {
+    var lowMessage = (" and lower case characters")
+  }  else {
+    var lowMessage = ("")
+  }
+
+  if(digits) {
+    var digMessage = (" and digits")
+  } else {
+    var digMessage = ("")
+  }
+
+  if(special) {
+    var specMessage = (" and special characters")
+  } else {
+    var specMessage = ("")
+  }
+
+  alert("Your password is " + length + " characters long," + upMessage + lowMessage + digMessage + specMessage + ".")
+
+
+  for ( let i = 0; i < length; i++) {
+    userPass.push(userSelect[Math.floor(Math.random() * userSelect.length)]);
+    var password = userPass.join('');
+  }
+  
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  console.log("the randomized password is: " + userPass)
+}
+
+//generating password
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
